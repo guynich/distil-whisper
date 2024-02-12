6d17e82f2fa8535fb80de02319f391a8fcf81f3c
Guy Nicholson <35084975+guynich@users.noreply.github.com>
2024-02-12 13:51:57 -0800

    Reduce dataloader_num_workers to 8
    Value of 16 caused freeze.

--- run_pseudo_labelling.sh
+++ run_pseudo_labelling.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# guynich workstation warning mitigation.
+TOKENIZERS_PARALLELISM=false
+
+# guynich workstation error and freeze mitigation.
+# Set dtype to float16 (not bfloat16).
+# Set preprocessing_num_workers 1 (not 16).
+# Set dataloader_num_workers 8 (not 16)
 accelerate launch training/run_pseudo_labelling.py \
   --model_name_or_path "openai/whisper-large-v2" \
   --dataset_name "mozilla-foundation/common_voice_13_0" \
@@ -11,7 +18,7 @@ accelerate launch training/run_pseudo_labelling.py \
   --wandb_project "distil-whisper-labelling" \
   --per_device_eval_batch_size 64 \
   --dtype "float16" \
-  --dataloader_num_workers 16 \
+  --dataloader_num_workers 8 \
   --preprocessing_num_workers 1 \
   --logging_steps 500 \
   --max_label_length 128 \
