d90f1a7172f14e551be370485fc1aec58a2cd558
Guy Nicholson <35084975+guynich@users.noreply.github.com>
2024-02-12 12:05:02 -0800

    Create run_pseudo_labelling.sh
    https://github.com/huggingface/distil-whisper/tree/main/training#1-pseudo-labelling

---
+++ run_pseudo_labelling.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+accelerate launch run_pseudo_labelling.py \
+  --model_name_or_path "openai/whisper-large-v2" \
+  --dataset_name "mozilla-foundation/common_voice_13_0" \
+  --dataset_config_name "hi" \
+  --dataset_split_name "train+validation+test" \
+  --text_column_name "sentence" \
+  --id_column_name "path" \
+  --output_dir "./common_voice_13_0_hi_pseudo_labelled" \
+  --wandb_project "distil-whisper-labelling" \
+  --per_device_eval_batch_size 64 \
+  --dtype "bfloat16" \
+  --dataloader_num_workers 16 \
+  --preprocessing_num_workers 16 \
+  --logging_steps 500 \
+  --max_label_length 128 \
+  --report_to "wandb" \
+  --language "hi" \
+  --task "transcribe" \
+  --return_timestamps \
+  --attn_type "flash_attn" \
+  --streaming False \
+  --generation_num_beams 1 \
+  --decode_token_ids False \
+  --push_to_hub
