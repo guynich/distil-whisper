797584d43e415a4cb49df80b0eebc5c1110a817c
Guy Nicholson <guy.nicholson@gmail.com>
2024-02-20 18:32:38 +0000

    Adds Librispeech WER evaluation script for Whisper Tiny model.

---
+++ training/scripts/run_short_form_eval_en_librispeech_tiny.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Adapted for Librispeech clean test with OpenAI Whisper Tiny in float32.
+# Compare https://huggingface.co/openai/whisper-tiny#evaluation WER 7.5%.
+
+# Mitigate out of memory on A10 GPU (23GB).
+#  --per_device_eval_batch_size 64 \
+
+# Mitigate `ConnectionError: Server Disconnected` on Lambdalabs instance.
+#  --streaming
+
+# WER 8.23607 % is higher than without the language option.
+#   --language "en" \
+
+# Mitigate tokenizer parallelism warning message.
+TOKENIZERS_PARALLELISM=false
+
+accelerate launch run_short_form_eval.py \
+  --model_name_or_path "openai/whisper-tiny" \
+  --dataset_name "librispeech_asr" \
+  --dataset_config_name "clean" \
+  --dataset_split_name "test" \
+  --text_column_name "text" \
+  --output_dir "./" \
+  --per_device_eval_batch_size 16 \
+  --dtype "float32" \
+  --dataloader_num_workers 16 \
+  --report_to "wandb" \
+  --generation_max_length 128 \
+  --attn_type "flash_attn"
